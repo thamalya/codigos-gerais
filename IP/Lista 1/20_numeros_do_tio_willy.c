6daba711626fb6024f4716390abf363e14f289e0
thamalya <142743879+thamalya@users.noreply.github.com>
2024-05-22 12:57:47 -0300

    finalizei codigo

---
+++ IP/Lista 1/20_numeros_do_tio_willy.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    int N;
+    scanf("%d", &N);
+    int n[20];
+
+    int  contador = 0;
+    int cont_parar = 0;
+
+    for(int i = 0; ; i++){
+        scanf("%d", &n[i]);
+        cont_parar++;
+        if(n[i] == -1 || cont_parar >= 20){
+            break;
+        }
+    }
+
+    for(int i = 0; i < cont_parar; i++){
+        if(N == n[i]){
+            contador++;
+        }
+    }
+
+    printf("%d aparece %d vezes", N, contador);
+    return 0;
+}
